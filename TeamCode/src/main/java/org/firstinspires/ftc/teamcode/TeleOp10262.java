78d05f94977dabef78fe6ddbfc65041f0905d676
Jacob Eason <31490880+JEason13@users.noreply.github.com>
2018-10-27 14:14:54 -0400

    Update TeleOp10262.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp10262.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp10262.java
@@ -5,7 +5,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
+//import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp (name = "TeleOp10262", group = "PioneerMiddleSchool")
 public class TeleOp10262 extends LinearOpMode
@@ -13,19 +13,25 @@ public class TeleOp10262 extends LinearOpMode
 
     private DcMotor motorLeft;
     private DcMotor motorRight;
+//    private Servo gripServo;
+    private DcMotor AFMotor;
+
 
     //public TeleOp10262() {
 
     //}
+
+
 
     @Override
     public void runOpMode()
     {
         DcMotor motorLeft = hardwareMap.dcMotor.get("motorLeft");
         DcMotor motorRight = hardwareMap.dcMotor.get("motorRight");
+        DcMotor AFMotor = hardwareMap.dcMotor.get("AFMotor");
 
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
-
+        waitForStart();
         while(opModeIsActive())
         {
 
@@ -35,19 +41,21 @@ public class TeleOp10262 extends LinearOpMode
 
             double drive = -gamepad1.left_stick_y;
             double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            if (gamepad1.a) {
-                motorLeft.setPower(0);
-                motorRight.setPower(0);
+            leftPower    = drive + turn;
+            rightPower   = drive - turn;
+            double scale = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+
+            if (scale > 1) {
+                scale = 1 / scale;
+                leftPower *= scale;
+                rightPower *= scale;
             }
 
-            if (gamepad1.y) {
-                motorLeft.setPower(1.0);
-                motorRight.setPower(1.0);
-            }
+            motorLeft.setPower(0.75 * leftPower);
+            motorRight.setPower(rightPower);
 
+            AFMotor.setPower(gamepad2.left_stick_x);
 
             idle();
 
