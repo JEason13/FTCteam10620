13f05f2bed65be2d8ad65fc9ebe62f44d6112c4b
Jacob Eason <31490880+JEason13@users.noreply.github.com>
2018-10-27 14:15:14 -0400

    Update TeleOp10620.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp10620.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp10620.java
@@ -5,15 +5,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
+//import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp (name = "TeleOp10620", group = "PioneerMiddleSchool")
 public class TeleOp10620 extends LinearOpMode
 {
 
-    private DcMotor motorLeft;
-    private DcMotor motorRight;
-    private DcMotor hexMotor;
+    private DcMotor motorLeftback;
+    private DcMotor motorRightback;
+    private DcMotor ABMotor;
+    //private DcMotor PullMotor;
+    private DcMotor ALMotor;
+
 
     //public TeleOp10620() {
 
@@ -22,11 +25,14 @@ public class TeleOp10620 extends LinearOpMode
     @Override
     public void runOpMode()
     {
-        DcMotor motorLeft = hardwareMap.dcMotor.get("motorLeft");
-        DcMotor motorRight = hardwareMap.dcMotor.get("motorRight");
-        DcMotor hexMotor = hardwareMap.dcMotor.get("hexMotor");
+        DcMotor motorLeftback = hardwareMap.dcMotor.get("motorLeftback");
+        DcMotor motorRightback = hardwareMap.dcMotor.get("motorRightback");
+        DcMotor ABMotor = hardwareMap.dcMotor.get("ABMotor");
+        DcMotor ALMotor = hardwareMap.dcMotor.get("ALMotor");
+        motorLeftback.setDirection(DcMotor.Direction.REVERSE);
+       // motorLeftfront.setDirection(DcMotor.Direction.REVERSE);
 
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        waitForStart();
 
         while(opModeIsActive())
         {
@@ -37,27 +43,31 @@ public class TeleOp10620 extends LinearOpMode
 
             double drive = -gamepad1.left_stick_y;
             double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            if (gamepad1.a) {
-                motorLeft.setPower(0);
-                motorRight.setPower(0);
+            leftPower    = drive + turn;
+            rightPower   = drive - turn;
+            double scale = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+
+            if (scale > 1) {
+                scale = 1 / scale;
+                leftPower *= scale;
+                rightPower *= scale;
             }
 
-            if (gamepad1.y) {
-                motorLeft.setPower(1.0);
-                motorRight.setPower(1.0);
+/*            if(gamepad2.x){
+                pullyServo.setPosition(360);
+            }else if (gamepad2.b){
+                pullyServo.setPosition(0);
             }
-
-            if(gamepad2.left_bumper){
-                hexMotor.setPower(1.0);
+            else{
+                pullyServo.setPosition(180);
             }
+*/
+            motorLeftback.setPower(leftPower);
+            motorRightback.setPower(rightPower);
 
-            if(gamepad2.right_bumper){
-                hexMotor.setPower(-1.0);
-            }
-
+            ABMotor.setPower(gamepad2.left_stick_y);
+            ALMotor.setPower(gamepad2.right_stick_x);
 
             idle();
 
